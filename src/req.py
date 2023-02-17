3217f42243e5afe3c64fdff2a13a9bdaeadb2d68
yaacovkdev <yaacov.kochatkov@gmail.com>
2023-02-17 15:50:04 -0800

    headless

--- src/req.py
+++ src/req.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 url = "https://www.bioinformatics.org/sms2/protein_gravy.html"
 searchterm = "textarea"
@@ -8,14 +9,16 @@ buttonname = "main_submit"
 #TODO: Find a better browser to communicate with, such as the one that doesnt use a GUI, so the server usage can be minimized
 
 #You have to have Firefox installed, Alternatively use webdriver.Chrome().
-driver = webdriver.Firefox()
+
+op = Options()
+op.add_argument('-headless')
+driver = webdriver.Firefox(options=op)
 
 driver.get(url)
 
 sbox = driver.find_element(By.TAG_NAME, searchterm)
 sbox.clear()
 sbox.send_keys(">sample sequence 2\nSEIIDNIYSVKAYCWESAMEKMIENLREVELKMTRKAAYMRFFTSSAFFFSGFFVVFLSVLPYTVINGIVLRKIFTTISFCIVLRMSVTRQFPTAVQIWYDSFGMIRKIQDFLQKQEYKVLEYNLMTTGI")
-
 
 button = driver.find_element(By.NAME, buttonname).click()
 driver.switch_to.window(driver.window_handles[1])
@@ -27,3 +30,4 @@ print(driver.find_element(By.CLASS_NAME, 'main').text)
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
 driver.close()
+driver.quit()
